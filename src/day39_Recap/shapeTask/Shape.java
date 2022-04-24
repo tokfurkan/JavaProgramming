aac4cdbb906bbb7ff224e5c0e0342c18dc243432
furkantok <furkant.ftk@gmail.com>
2022-04-24 23:55:42 +0300

    day39: Recap

---
+++ src/day39_Recap/shapeTask/Shape.java
@@ -0,0 +1,41 @@
+package day39_Recap.shapeTask;
+
+public class Shape {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+
+        if (name == null){
+            System.err.println("Name cannot be null");
+            System.exit(1); // 1: something went wrong
+        }
+
+        if (name.isEmpty() || name.isBlank()){
+            System.err.println("Invalid name");
+            System.exit(1);
+        }
+
+        this.name = name;
+    }
+
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    public double area(){
+        return 0;
+    }
+
+    public double perimeter(){
+        return 0;
+    }
+
+
+
+
+}
