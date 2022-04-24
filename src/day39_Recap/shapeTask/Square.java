ab33bba30f886ca7477510efff77c940d5e3b103
furkantok <furkant.ftk@gmail.com>
2022-04-25 00:15:23 +0300

    day39: Recap

--- src/day39_Recap/shapeTask/Square.java
+++ src/day39_Recap/shapeTask/Square.java
@@ -22,4 +22,23 @@ public class Square extends Shape{
         super("Square");
         setSide(side);
     }
+
+    @Override
+    public double area() {
+        return side * side;
+    }
+
+    @Override
+    public double perimeter() {
+        return side * 4;
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" +
+                "side=" + side +
+                ", area='" + area() + '\'' +
+                ", perimeter='" + perimeter() + '\'' +
+                '}';
+    }
 }
