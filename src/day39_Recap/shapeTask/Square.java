8a6cf552ca134ce5bd9f5f739eb9c7d3b7afa499
furkantok <furkant.ftk@gmail.com>
2022-04-25 00:06:21 +0300

    day39: Recap

--- src/day39_Recap/shapeTask/Square.java
+++ src/day39_Recap/shapeTask/Square.java
@@ -1,4 +1,25 @@
 package day39_Recap.shapeTask;
 
-public class Square {
+public class Square extends Shape{
+
+    private double side;
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+
+        if (side <= 0){
+            System.out.println("Invalid side: " + side);
+            System.exit(1);
+        }
+
+        this.side = side;
+    }
+
+    public Square(String name, double side) {
+        super("Square");
+        setSide(side);
+    }
 }
