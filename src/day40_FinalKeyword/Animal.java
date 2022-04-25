e6c4723e140da9497a2d11b478520f14e5b0ccf7
furkantok <furkant.ftk@gmail.com>
2022-04-26 00:42:09 +0300

    day40: FinalVariable Animal and Dog Classes

--- src/day40_FinalKeyword/Animal.java
+++ src/day40_FinalKeyword/Animal.java
@@ -57,4 +57,16 @@ public class Animal {
     public void setAge(int age) {
         this.age = age;
     }
+
+    //methods
+
+    public void eat(){
+        System.out.println(name + " is eating");
+    }
+    public final void drink(){
+        System.out.println(name + " is drinking water");
+    }
+
+
+
 }
---
+++ src/day40_FinalKeyword/Dog.java
@@ -0,0 +1,24 @@
+package day40_FinalKeyword;
+
+public class Dog extends Animal{
+
+    public Dog(String name, String breed, char gender, String color, String size, int age) {
+        super(name, breed, gender, color, size, age);
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(getName() + " is eating dog food");
+    }
+
+
+    //public void drink() { // final methods cannot be overridden
+        //System.out.println(getName() + " is drinking beer");
+
+    public void bark(){
+        System.out.println(getName() + " is barking");
+    }
+
+
+}
+
