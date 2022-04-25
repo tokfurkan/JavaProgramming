6a8f34f4ba3fee17fdccaae52c05b82ca3cd749a
furkantok <furkant.ftk@gmail.com>
2022-04-25 12:18:09 +0300

    day39: Recap Cydeo Class

---
+++ src/day39_Recap/cydeoTask/Cydeo.java
@@ -0,0 +1,62 @@
+package day39_Recap.cydeoTask;
+
+public class Cydeo {
+
+    public static void main(String[] args) {
+
+        Developer developer = new Developer("Korkmaz", 29, 'M', 1, "Java Developer", 125000);
+
+        Tester tester = new Tester("Olga", 27, 'F', 2, "SDET", 11000);
+
+        Teacher teacher = new Teacher("Daniel", 32, 'M', 3, "Math Teacher", 100000);
+
+        Student student = new Student("Shuaib", 30, 'M', 8, "SDET");
+
+        System.out.println(developer);
+        System.out.println(tester);
+        System.out.println(teacher);
+        System.out.println(student);
+
+        System.out.println("-------------------------------------------");
+
+        developer.setAge(39);
+        System.out.println(developer.getAge());
+        System.out.println(developer);
+
+        System.out.println("--------------------------------------");
+
+        developer.work();
+        tester.work();
+        teacher.work();
+
+        System.out.println("--------------------------------------");
+
+        developer.eat();
+        developer.drink();
+        developer.sleep();
+        developer.fixingBugs();
+        tester.createTicket();
+
+        System.out.println("--------------------------------------");
+
+        teacher.eat();
+        teacher.drink();
+        teacher.sleep();
+
+        System.out.println("--------------------------------------");
+
+        student.eat();
+        student.drink();
+        student.sleep();
+        student.study();
+
+
+
+
+
+
+
+
+
+    }
+}
