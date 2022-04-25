ab7bc001b468a96d7b1c461b925c2b65b050e65b
furkantok <furkant.ftk@gmail.com>
2022-04-26 00:59:17 +0300

    day40: FinalKeyword last commit

--- src/day40_FinalKeyword/Dog.java
+++ src/day40_FinalKeyword/Dog.java
@@ -1,6 +1,6 @@
 package day40_FinalKeyword;
 
-public class Dog extends Animal{
+public final class Dog extends Animal{
 
     public Dog(String name, String breed, char gender, String color, String size, int age) {
         super(name, breed, gender, color, size, age);
