1ecdbfa4cf9f3b78ec1470064dca7ee3b49e3b7f
furkantok <furkant.ftk@gmail.com>
2022-04-28 16:03:52 +0300

    day42: ThrowKeyword 1 & 2

---
+++ src/day42_Exceptions/ThrowKeyword2.java
@@ -0,0 +1,18 @@
+package day42_Exceptions;
+
+import java.util.NoSuchElementException;
+
+public class ThrowKeyword2 {
+
+    public static void main(String[] args) {
+
+        throw new NoSuchElementException("There is no such element as break");
+
+
+
+
+
+
+
+    }
+}
