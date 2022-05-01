d2e5b2dadde6864e46f8a2070c15610d447dcc3b
furkantok <furkant.ftk@gmail.com>
2022-05-01 14:55:55 +0300

    day43: employeeTask.Employee

---
+++ src/day43_Abstraction/employeeTask/Employee.java
@@ -0,0 +1,52 @@
+package day43_Abstraction.employeeTask;
+
+public abstract class Employee extends Person {
+
+    //variables
+    private final int id;
+    private String jobTitle;
+    private double salary;
+
+    public Employee(String name, int age, char gender, int id, String jobTitle, double salary) {
+        super(name, age, gender);
+        this.id = id;
+        this.jobTitle = jobTitle;
+        this.salary = salary;
+    } //parent class' constructor
+
+    //getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    //abstract method
+    public abstract void work();
+
+    @Override //overridden toString
+    public String toString() {
+        return "Employee{" +
+                "name=" + getName() +
+                ", age=" + getAge() +
+                ", gender=" + getGender() +
+                ", id=" + id +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
