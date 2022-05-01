06bfd282a34f04aa24dc6f6af357d71b833134a2
furkantok <furkant.ftk@gmail.com>
2022-05-01 18:36:11 +0300

    day44: animalTask.Dog

---
+++ src/day44_Abstraction/animalTask/Dog.java
@@ -0,0 +1,22 @@
+package day44_Abstraction.animalTask;
+
+public final class Dog extends Animal{
+
+    public Dog(String name, String breed, char gender, int age, String size, String color) {
+        super(name, breed, gender, age, size, color);
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(getName() + " is eating Pizza");
+    }
+
+    public void bark(){
+        System.out.println(getName() + " is barking");
+    }
+
+
+
+
+
+}
