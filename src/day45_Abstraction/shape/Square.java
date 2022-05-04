8de62e788dfec339de76de6952db3eaf4a9d6bd7
furkantok <furkant.ftk@gmail.com>
2022-05-05 02:41:02 +0300

    day45: shapeTask.square

---
+++ src/day45_Abstraction/shape/Square.java
@@ -0,0 +1,41 @@
+package day45_Abstraction.shape;
+
+public class Square extends Shape{
+
+    private double side;
+
+    public Square(String name, double side) {
+        super("Square");
+        setSide(side);
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        if (side <= 0){
+            throw new RuntimeException("Invalid side value: " + side);
+        }
+        this.side = side;
+    }
+
+
+    @Override
+    public double area() {
+        return side * side;
+    }
+
+    @Override
+    public double perimeter() {
+        return side * 4;
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" +
+                super.toString() +
+                "side=" + side +
+                '}';
+    }
+}
