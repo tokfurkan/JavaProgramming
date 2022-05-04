e9fa6bf8c6eb43e177b96bdb78f03f4a0a41d083
furkantok <furkant.ftk@gmail.com>
2022-05-05 02:56:26 +0300

    day45: Extends_vs_Implements

--- src/day45_Abstraction/Extends_vs_Implements.java
+++ src/day45_Abstraction/Extends_vs_Implements.java
@@ -1,4 +1,38 @@
 package day45_Abstraction;
 
+//extends:
+class C{
+
+}
+
+class A{
+
+}
+
+//class B extends A, C{} multiple inheritence not allowed between classes
+
+interface Z{
+
+}
+
+interface X{
+
+}
+
+interface Y extends X, Z{
+    //multiple inheritance is allowed in interfaces
+}
+
+//implements
+class D implements X, Y, Z{
+
+}
+//interfaces can never inherit from classes
+
+
+
 public class Extends_vs_Implements {
+
+
+
 }
