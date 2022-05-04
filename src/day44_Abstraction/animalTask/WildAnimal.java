e96f8535060fc1a8076ef74758c06ab326a54c8f
furkantok <furkant.ftk@gmail.com>
2022-05-04 23:22:52 +0300

    day44: animalTask.interface

---
+++ src/day44_Abstraction/animalTask/WildAnimal.java
@@ -0,0 +1,11 @@
+package day44_Abstraction.animalTask;
+
+public interface WildAnimal {
+
+    //access modifiers are public by default
+
+    boolean isWild = true; //static and final by default
+
+    void hunt(); //abstract by default
+
+}
